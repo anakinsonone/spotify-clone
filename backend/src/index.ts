cfda02ce68a6712352ae0920240c1212523faf07
anakinsonone <akkhi.sonone@gmail.com>
2024-07-08 23:06:05 +0530

    init basic typescript server

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+import "dotenv/config";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + Typescript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running on http://localhost:${port}`);
+});
