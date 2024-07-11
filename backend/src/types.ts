f39671095160a830e62c3fe2ac141110691347a9
anakinsonone <akkhi.sonone@gmail.com>
2024-07-11 21:30:26 +0530

    add input types for artists, albums and songs

---
+++ backend/src/types.ts
@@ -0,0 +1,5 @@
+import { Prisma } from "@prisma/client";
+
+export type AlbumsCreateInput = Prisma.albumsCreateInput;
+export type ArtistsCreateInput = Prisma.artistsCreateInput;
+export type SongsCreateInput = Prisma.songsCreateInput;
