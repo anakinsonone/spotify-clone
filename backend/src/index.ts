4345b0f03d49b94bf7b98aa765bf188c77f84db8
anakinsonone <akkhi.sonone@gmail.com>
2024-07-11 21:34:02 +0530

    add error handling middleware to server, add routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import bodyParser from "body-parser";
 import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+
+import errorHandler from "./middlewares/errorMiddleware";
+import { AlbumRouter, ArtistsRouter, SongsRouter } from "./routes";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +13,12 @@ app.use(bodyParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + Typescript Server");
 });
+
+app.use("/artists", ArtistsRouter);
+app.use("/albums", AlbumRouter);
+app.use("/songs", SongsRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running on http://localhost:${port}`);
