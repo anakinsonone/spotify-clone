26d41eb9c972e155fe4dc86bd3d728cc164ce1a7
anakinsonone <akkhi.sonone@gmail.com>
2024-07-24 20:51:51 +0530

    add userRoutes to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import express, { Express, Request, Response } from "express";
 
-import errorHandler from "./middlewares/errorMiddleware";
-import { AlbumRouter, ArtistsRouter, SongsRouter } from "./routes";
+import { errorHandler } from "./middlewares";
+import { AlbumRouter, ArtistsRouter, SongsRouter, UserRouter } from "./routes";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + Typescript Server");
@@ -16,9 +17,10 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/artists", ArtistsRouter);
 app.use("/albums", AlbumRouter);
 app.use("/songs", SongsRouter);
+app.use("/users", UserRouter);
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running on http://localhost:${port}`);
+  console.log(`[server] Server is running on http://localhost:${port}`);
 });
