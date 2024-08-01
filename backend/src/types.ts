efcf161037c64642a613362db39a3ae5d1e32f38
anakinsonone <akkhi.sonone@gmail.com>
2024-08-01 23:04:23 +0530

    define user property in session.

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,5 +1,12 @@
 import { Prisma } from "@prisma/client";
+import "express-session";
 
 export type AlbumsCreateInput = Prisma.albumsCreateInput;
 export type ArtistsCreateInput = Prisma.artistsCreateInput;
 export type SongsCreateInput = Prisma.songsCreateInput;
+
+declare module "express-session" {
+  interface SessionData {
+    user?: { id: string };
+  }
+}
