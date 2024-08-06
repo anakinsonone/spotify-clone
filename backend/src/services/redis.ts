95741a21e7e6343850b0a9754a5f5bb271817c21
anakinsonone <akkhi.sonone@gmail.com>
2024-08-06 14:23:21 +0530

    remove separate redis client

--- backend/src/services/redis.ts
+++
@@ -1,6 +0,0 @@
-import Redis from "ioredis";
-
-export const redisClient = new Redis({
-  host: process.env.REDIS_STORE_HOST as string,
-  port: parseInt(process.env.REDIS_STORE_PORT as string),
-});
