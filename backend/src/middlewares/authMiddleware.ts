769d452f86a8d78cd519e76d6607a3064d26f453
anakinsonone <akkhi.sonone@gmail.com>
2024-08-13 16:54:56 +0530

    update auth middleware

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import { CustomError } from "./errorMiddleware";
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
   if (req.session && req.session.isLoggedIn) {
     next();
   } else {
-    res.status(401).json("Unauthorized: Please log in to access this resource");
+    const error = new Error(
+      "Unauthorized: Please log in to access this resource.",
+    );
+    (error as CustomError).status = 401;
+    next(error);
   }
 };
