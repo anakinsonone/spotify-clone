45486a1ef64085283de2e0687d2389e15a2facd8
anakinsonone <akkhi.sonone@gmail.com>
2024-08-16 20:22:24 +0530

    create route for playing requested song

--- backend/src/routes/songRouter.ts
+++ backend/src/routes/songRouter.ts
@@ -49,3 +49,46 @@ SongsRouter.post("/create", upload.single("song"), async (req, res, next) => {
     next(error);
   }
 });
+
+SongsRouter.get("/:song_id", async (req, res, next) => {
+  try {
+    const song_id = req.params.song_id;
+    const songPath = await prisma.songs.findUnique({
+      where: {
+        song_id: parseInt(song_id),
+      },
+      select: {
+        path: true,
+      },
+    });
+
+    if (!songPath || !songPath?.path) {
+      throw { status: 404, message: "Song not found" };
+    }
+
+    const filePath = songPath.path;
+    const CHUNK_SIZE = 500 * 1e3;
+
+    const range = req.headers.range || "0";
+    const fileSize = statSync(filePath).size;
+
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, fileSize - 1);
+    const contentLength = end - start + 1;
+
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "audio/mpeg",
+      "Transfer-Encoding": "chunked",
+    };
+
+    res.writeHead(206, headers);
+
+    const stream = createReadStream(filePath, { start, end });
+    stream.pipe(res);
+  } catch (error) {
+    next(error);
+  }
+});
