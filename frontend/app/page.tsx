0ac9319b5afd45b1f67e732d73145615ac59c90f
anakinsonone <akkhi.sonone@gmail.com>
2024-08-23 15:01:03 +0530

    add navbar component to page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Navbar from "./components/Navbar";
 
 const app = () => {
-  return <div>app</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default app;
